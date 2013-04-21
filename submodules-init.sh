9ce2e3406087d1b47d95fc710f705ebe46b9eae6
Andrei Sebastian Cîmpean <andreiamenta@gmail.com>
2013-04-21 22:31:33 +0300

    Update repo and clean.

--- submodules-init.sh
+++ submodules-init.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+rm -rf bundle/*
+git rm --cached bundle/*
+rm -rf .git/modules/bundle
+
 git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
     while read path_key path
     do
@@ -9,4 +13,5 @@ git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
         url=$(git config -f .gitmodules --get "$url_key")
         git submodule add $url $path
     done
-git update-index --assume-unchaged bundle   
+
+git update-index --assume-unchanged bundle
